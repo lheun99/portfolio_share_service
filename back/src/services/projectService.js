d6c996fdbe7bec73860bc11dff0af24fd35f6658
Lee Ha-eun <lheun99@gmail.com>
2022-03-16 15:59:57 +0900

    ADD : PUT projects/:id 기능

--- back/src/services/projectService.js
+++ back/src/services/projectService.js
@@ -39,6 +39,44 @@ class projectAuthService {
 
     return project;
   }
+
+  static async setProject({ project_id, toUpdate }) {
+    // 우선 해당 id 의 유저가 db에 존재하는지 여부 확인
+
+    let project = await Project.findByProjectId({ project_id });
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!project) {
+      const errorMessage =
+        "프로젝트가 존재하지 않습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+
+    // 업데이트 대상에 name이 있다면, 즉 name 값이 null 이 아니라면 업데이트 진행
+    if (toUpdate.title) {
+      const fieldToUpdate = "title";
+      const newValue = toUpdate.title;
+      project = await Project.update({ project_id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.description) {
+      const fieldToUpdate = "description";
+      const newValue = toUpdate.description;
+      project = await Project.update({ project_id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.from_date) {
+      const fieldToUpdate = "from_date";
+      const newValue = toUpdate.from_date;
+      project = await Project.update({ project_id, fieldToUpdate, newValue });
+    }
+
+    if (toUpdate.to_date) {
+      const fieldToUpdate = "to_date";
+      const newValue = toUpdate.to_date;
+      project = await Project.update({ project_id, fieldToUpdate, newValue });
+    }
+    return project;
+  }
 }
 
 export { projectAuthService };
