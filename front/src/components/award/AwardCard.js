e24e22fc3e6b4b219229b5be6291bf421a276e32
singahyeon12 <singahyeon12@naver.com>
2022-03-17 17:07:52 +0900

    Update: AwardCard 컴포넌트
    - 화살표 함수로 수정
    - useContext를 이용하여 user의 id를 받아오도록 수정
    - 삭제 버튼을 누르면 award를 delete하는 handleDelete 함수가 실행되도록
      추가
    - handleDelete: 해당 award의 id로 DELETE 요청을 하고 userid로 GET 요청
    - Container style 추가

--- front/src/components/award/AwardCard.js
+++ front/src/components/award/AwardCard.js
@@ -1,10 +1,29 @@
 import { Row, Button, Col, Container, Card } from "react-bootstrap";
+import { useContext } from 'react';
+import * as Api from "../../api";
+import { UserStateContext } from "../../App";
 
 // Award 컴포넌트에서 award를 입력받아서 award의 title과 description을 나타냄.
 // isEditable이 true일 경우, 편집 버튼을 생성함.
-function AwardCard({ award, setIsEditing, isEditable }) {
+const AwardCard = ({ award, setIsEditing, isEditable, setAwards }) => {
+
+  // useContext를 이용하여 user의 id를 받아옴
+  const userState = useContext(UserStateContext);
+  const userId = userState.user.id;
+
+  // award를 delete하는 함수
+  const handleDelete = async () => {
+      console.log('삭제 시도')
+      console.log(`award id는 ${award.id}`)
+
+      await Api.delete(`awards/${award.id}`);
+
+      await Api.get("awardlist", userId).then((res) => setAwards(res.data));
+  }
+
+  // award 상세 목록 및 편집, 삭제 버튼 생성
   return (
-    <Container>
+    <Container style={{margin:10, padding: 10,}}>
       <Row>
         <Col sm={11}>
           <Card.Subtitle>{award?.title}</Card.Subtitle>
@@ -18,6 +37,15 @@ function AwardCard({ award, setIsEditing, isEditable }) {
                 onClick={() => setIsEditing(true)}
               >
                 편집
+              </Button>
+          )}
+          {isEditable && (
+              <Button
+                variant="outline-danger"
+                size="sm"
+                onClick={handleDelete}
+              >
+                삭제
               </Button>
           )}
         </Col>
