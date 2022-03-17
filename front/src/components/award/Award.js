9fb85ae897b6d3961fe35f1779627918a0564479
singahyeon12 <singahyeon12@naver.com>
2022-03-17 16:59:43 +0900

    Update: Award 컴포넌트
    화살표 함수로 수정
    Award 함수 파라미터 수정
    AwardEditForm과 AwardCard에 setAwards 파라미터 전달
    useState import

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -1,12 +1,12 @@
 // 각 Award 컴포넌트는 isEditing 상태에 따라, false면 AwardCard, true면 AwardEditForm이 됩니다.
 
-import React from "react";
+import React, {useState} from "react";
 import AwardEditForm from "./AwardEditForm";
 import AwardCard from "./AwardCard";
 
-function Award({ award, isEditable, isEditing,  setIsEditing }) {
+const Award = ({ award, isEditable, setAwards}) => {
   // useState 훅을 통해 isEditing 상태를 생성함.
-  // const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
 // isEditing이 true이면 AwardEditForm을 생성하고, false이면 AwardCard를 생성함.
   return (
@@ -15,12 +15,14 @@ function Award({ award, isEditable, isEditing,  setIsEditing }) {
             <AwardEditForm
             award={award}
             setIsEditing={setIsEditing}
+            setAwards={setAwards}
             />
         ) : (
             <AwardCard
             award={award}
             setIsEditing={setIsEditing}
             isEditable={isEditable}
+            setAwards={setAwards}
             />
         )}
     </>
