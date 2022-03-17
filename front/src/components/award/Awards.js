c45fc922b3b8dc7140ef90bf05dae267face0034
singahyeon12 <singahyeon12@naver.com>
2022-03-17 17:49:17 +0900

    Update: Awards 컴포넌트
    - 오류 발견: useContext로 userid를 받아왔더니 다른 유저로 들어와도 다
    같은 내용이 생성됨
    - useContext 삭제
    - portfolioOwnerId를 파라미터로 받아와서 사용
    - 추가 버튼은 해당 유저만 접근할 수 있도록 설정
    - 리턴하는 모든 컴포넌트에 portfolioOwnerId 전달

--- front/src/components/award/Awards.js
+++ front/src/components/award/Awards.js
@@ -1,31 +1,27 @@
 // 수상이력 목록
 // 여러 개의 Award 컴포넌트 + (추가하기 버튼 클릭 시) AwardAddForm 컴포넌트로 구성됩니다.
 
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
 
 import * as Api from "../../api";
 import Award from "./Award";
 import AwardAddForm from "./AwardAddForm";
-import { UserStateContext } from "../../App";
 
-const Awards = ({isEditable}) => {
+const Awards = ({portfolioOwnerId, isEditable}) => {
   // useState 훅을 통해 isAdding 상태를 생성함.
   const [isAdding, setIsAdding] = useState(false);
   // useState 훅을 통해 awards 상태를 생성함.
   const [awards, setAwards] = useState([])
-  // useContext를 이용하여 user의 id를 받아옴
-  const userState = useContext(UserStateContext);
-  const userId = userState.user.id;
 
 // "awardlist"로 GET 요청을 하고, awards를 response의 data로 세팅함.
   useEffect(() => {
     const fetch = async () => {
-      const res = await Api.get(`awardlist/${userId}`);
+      const res = await Api.get(`awardlist/${portfolioOwnerId}`);
       setAwards(res.data)
     }
     fetch();
-  }, [userId]);
+  }, [portfolioOwnerId]);
 
 // title은 '수상이력', body는 Award 컴포넌트의 집합으로 이루어진 Card를 생성함.
 // 버튼을 클릭하면 isAdding이 true로 바뀌며, AwardAddForm 컴포넌트가 생성됨.
@@ -35,12 +31,12 @@ const Awards = ({isEditable}) => {
       <Card.Body>
         <Card.Title>수상이력</Card.Title>
         {awards && (awards.map((award) => (
-          <Award key={award.id} award={award} isEditable={isEditable} setAwards={setAwards} />)
+          <Award key={award.id} portfolioOwnerId={portfolioOwnerId} award={award} isEditable={isEditable} setAwards={setAwards} />)
         ))}
         <div style={{ textAlign: "center" }}>
-          <Button variant="primary" onClick={() => setIsAdding(true)}>+</Button>
+          {isEditable && <Button variant="primary" onClick={() => setIsAdding(true)}>+</Button>}
         </div>
-        {isAdding && <AwardAddForm awards={awards} setAwards={setAwards} setIsAdding={setIsAdding} />}
+        {isAdding && <AwardAddForm awards={awards} portfolioOwnerId={portfolioOwnerId} setAwards={setAwards} setIsAdding={setIsAdding} />}
       </Card.Body>
     </Card>
   );
