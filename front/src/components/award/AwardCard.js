caa553170ae4bbaec05c060dd31a0c8cb2914580
singahyeon12 <singahyeon12@naver.com>
2022-03-17 13:40:40 +0900

    Update: AwardCard 컴포넌트
    Card 부트스트랩 추가해서 title과 text로 사용함
    ^M크기 변경

--- front/src/components/award/AwardCard.js
+++ front/src/components/award/AwardCard.js
@@ -1,16 +1,16 @@
-import { Row, Button, Col, Container } from "react-bootstrap";
+import { Row, Button, Col, Container, Card } from "react-bootstrap";
 
 // Award 컴포넌트에서 award를 입력받아서 award의 title과 description을 나타냄.
 // isEditable이 true일 경우, 편집 버튼을 생성함.
 function AwardCard({ award, setIsEditing, isEditable }) {
   return (
-    <Container fluid>
-      <Row md="9">
-        <Col className>
-          {award?.title}<br/>
-          {award?.description}
+    <Container>
+      <Row>
+        <Col sm={11}>
+          <Card.Subtitle>{award?.title}</Card.Subtitle>
+          <Card.Text className="text-muted">{award?.description}</Card.Text>
         </Col>
-        <Col md="3">
+        <Col sm={1}>
           {isEditable && (
               <Button
                 variant="outline-info"
