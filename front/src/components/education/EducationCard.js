69c9cb8425d2b533eb96c998d886e0d7bd8a141b
joohe71 <joohe71@gmail.com>
2022-03-18 22:45:49 +0900

    Fix : POST 요청 함수 수정, 주석처리 및 전체적인 코드 정리

--- front/src/components/education/EducationCard.js
+++ front/src/components/education/EducationCard.js
@@ -1,44 +1,49 @@
-import { Row, Col, Container, Card, Button,ButtonGroup } from "react-bootstrap";
+import { Row, Col, Container, Card, Button, ButtonGroup } from "react-bootstrap";
 import { useState } from 'react';
 import EducationEditForm from './EducationEditForm';
 
 // 학력 정보 조회 상세 값 컴포넌트
-const EducationCard = ({value, editHandler, deleteHandler, isEditable}) => {
-    const { id, school, major, position } = value;
-    const [isEditing, SetIsEditing] = useState(false);
+const EducationCard = ({ value, editHandler, deleteHandler, isEditable }) => {
+	const { id, school, major, position } = value;
+	const [isEditing, SetIsEditing] = useState(false);
 
-    const updateHandler = () => {
-        SetIsEditing(!isEditing)
-    };
+	// 수정 form show 및 hide 처리용 함수
+	const updateHandler = () => {
+		SetIsEditing(!isEditing);
+	};
 
-    const handleDelete = (e) => {
-        e.preventDefault()
-        deleteHandler(id, e.target.value)
-    }
+	// 삭제 처리 함수
+	const handleDelete = (e) => {
+		e.preventDefault();
 
-    return (
-        isEditing ? 
-        <EducationEditForm 
-            item={value} 
-            editHandler={editHandler} 
-            onUpdate={updateHandler}/> : 
-        <Container style={{margin:10, padding: 10,}}>
-            <Row>
-                <Col sm={10}>
-                    <Card.Subtitle>{school}</Card.Subtitle>
-                    <Card.Text className="text-muted">{major +  ` (${position})`}</Card.Text>
-                </Col>
+		// Education 컴포넌트에서 받아온 delete 요청 함수
+		deleteHandler(id, e.target.value);
+	};
 
-                {isEditable ? 
-                    <Col sm={2}>
-                        <ButtonGroup style={{margin: 10,}} size='sm'>
-                            <Button variant="outline-info" onClick={updateHandler}>편집</Button>
-                            <Button variant="outline-danger" onClick={handleDelete}>삭제</Button>
-                        </ButtonGroup>
-                    </Col> : <></>}
-            </Row>
-        </Container>
-        )
+	// 로그인 유저는 자신의 페이지만 추가/편집/삭제 가능  
+	return (
+		isEditing ?
+			<EducationEditForm
+				item={value}
+				editHandler={editHandler}
+				onUpdate={updateHandler} /> :
+			<Container style={{ margin: 10, padding: 10, }}>
+				<Row>
+					<Col sm={10}>
+						<Card.Subtitle>{school}</Card.Subtitle>
+						<Card.Text className="text-muted">{major + ` (${position})`}</Card.Text>
+					</Col>
+
+					{isEditable ?
+						<Col sm={2}>
+							<ButtonGroup style={{ margin: 10, }} size='sm'>
+								<Button variant="outline-info" onClick={updateHandler}>편집</Button>
+								<Button variant="outline-danger" onClick={handleDelete}>삭제</Button>
+							</ButtonGroup>
+						</Col> : <></>}
+				</Row>
+			</Container>
+	);
 };
 
 export default EducationCard;
--- front/src/components/education/EducationCardList.js
+++ front/src/components/education/EducationCardList.js
@@ -1,13 +1,20 @@
 import EducationCard from "./EducationCard";
 
 // 학력 정보 조회 list 컴포넌트
-const EducationCardList = ({topics, editHandler, deleteHandler, isEditable}) => {
-
-    return (
-        <>
-            {topics.map((item) => <EducationCard key={item.id} value={item} editHandler={editHandler} deleteHandler={deleteHandler} isEditable={isEditable}/>)}
-        </>        
-    )
+const EducationCardList = ({ topics, editHandler, deleteHandler, isEditable }) => {
+	return (
+		<>
+			{topics
+				.map((item) => 
+					<EducationCard 
+						key={item.id} 
+						value={item} 
+						editHandler={editHandler} 
+						deleteHandler={deleteHandler} 
+						isEditable={isEditable} 
+						/>)}
+		</>
+	);
 
 };
 
