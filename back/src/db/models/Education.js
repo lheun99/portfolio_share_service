9c0f150efc8d0d6e267ac481bf14434c3f8f48fe
Hye-S <gptjs7970@naver.com>
2022-03-18 18:12:39 +0900

    Fix&Add: education-back 코드 리팩토링 및 validation 미들웨어 추가 fix: 변수명 및 함수 형태(화살표함수) 로 리팩토링 하였습니다. Add: 백엔드 단에서의  데이터 유효성 검사 미들웨어를 작성하였습니다.

--- back/src/db/models/Education.js
+++ back/src/db/models/Education.js
@@ -1,22 +1,26 @@
 import { EducationModel } from "../schemas/education";
 
+// Education DB CRUD 기능 구현
 class Education {
+  // 새로운 data를 추가
   static async create({ newEducation }) {
     const createdNewEducation = await EducationModel.create(newEducation);
     return createdNewEducation;
   }
 
+  // data의 ID를 통해 검색
   static async findById({ educationId }) {
     const education = await EducationModel.findOne({ id: educationId });
     return education;
   }
 
+  // user_id가 같은 모든 data 검색
   static async findByUserId({ userId }) {
-    console.log(userId);
     const educations = await EducationModel.find({ user_id: userId });
     return educations;
   }
-
+  
+  // filter에 맞는 data의 변경할 필드(fieldToUpdate)에 변경할 내용(newValue)을 업데이트
   static async update({ educationId, fieldToUpdate, newValue }) {
     const filter = { id: educationId };
     const toUpdate = { [fieldToUpdate]: newValue };
@@ -30,6 +34,7 @@ class Education {
     return updatedEducation;
   }
 
+  // id를 이용하여 data 삭제
   static async delete({ educationId }) {
     const deletedEducation = await EducationModel.deleteOne({ id: educationId });
     return deletedEducation;
