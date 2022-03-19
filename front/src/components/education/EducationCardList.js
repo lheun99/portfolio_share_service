9c8e39937088fe431237932904ef0f2d3b4531f8
joohe71 <joohe71@gmail.com>
2022-03-19 20:15:35 +0900

    코드 탭 간격 조정

--- front/src/components/education/EducationCardList.js
+++ front/src/components/education/EducationCardList.js
@@ -2,19 +2,19 @@ import EducationCard from "./EducationCard";
 
 // 학력 정보 조회 list 컴포넌트
 const EducationCardList = ({ topics, editHandler, deleteHandler, isEditable }) => {
-	return (
-		<>
-			{topics
-				.map((item) => 
-					<EducationCard 
-						key={item.id} 
-						value={item} 
-						editHandler={editHandler} 
-						deleteHandler={deleteHandler} 
-						isEditable={isEditable} 
-					/>)}
-		</>
-	);
+  return (
+    <>
+      {topics
+        .map((item) => 
+          <EducationCard 
+            key={item.id} 
+            value={item} 
+            editHandler={editHandler} 
+            deleteHandler={deleteHandler} 
+            isEditable={isEditable} 
+          />)}
+    </>
+  );
 
 };
 
