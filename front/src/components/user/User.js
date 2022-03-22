783b0137536a2421f9007de8625c75f9ecc365cf
singahyeon12 <singahyeon12@naver.com>
2022-03-22 13:40:51 +0900

    Update: User
    - user 편집을 ProfilePage에서 관리함에 따라 isEditing 변수와
    UserEditForm 컴포넌트를 삭제하고 UserCard와 합침

--- front/src/components/user/User.js
+++ front/src/components/user/User.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
-import UserEditForm from "./UserEditForm";
-import UserCard from "./UserCard";
+import { useNavigate } from "react-router-dom";
+import { Card, Row } from "react-bootstrap";
 import * as Api from "../../api";
 
 function User({ portfolioOwnerId, isEditable }) {
-  // useState 훅을 통해 isEditing 상태를 생성함.
-  const [isEditing, setIsEditing] = useState(false);
   // useState 훅을 통해 user 상태를 생성함.
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     // "users/유저id" 엔드포인트로 GET 요청을 하고, user를 response의 data로 세팅함.
@@ -15,21 +14,25 @@ function User({ portfolioOwnerId, isEditable }) {
   }, [portfolioOwnerId]);
 
   return (
-    <>
-      {isEditing ? (
-        <UserEditForm
-          user={user}
-          setIsEditing={setIsEditing}
-          setUser={setUser}
-        />
-      ) : (
-        <UserCard
-          user={user}
-          setIsEditing={setIsEditing}
-          isEditable={isEditable}
-        />
-      )}
-    </>
+    <Card 
+      onClick={() => isEditable ? navigate(`/users/${user.id}/userPage`) : undefined} 
+      className="mb-2 ms-3 mr-5" 
+      style={{ width: "18rem", cursor: "pointer" }}
+    >
+      <Card.Body>
+        <Row className="justify-content-md-center">
+          <Card.Img
+            style={{ width: "10rem", height: "8rem" }}
+            className="mb-3"
+            src="http://placekitten.com/200/200"
+            alt="랜덤 고양이 사진 (http://placekitten.com API 사용)"
+          />
+        </Row>
+        <Card.Title>{user?.name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{user?.email}</Card.Subtitle>
+        <Card.Text>{user?.description}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
