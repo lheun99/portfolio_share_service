d7faef0026f753ceedc4a602f72c03fce0cd1a17
singahyeon12 <singahyeon12@naver.com>
2022-03-23 18:27:34 +0900

    Update: ProjectList
    - 받아온 portfolioOwnerId로 PUT 요청해서 projectList.length를
    projectNum로 넘겨줌

--- front/src/components/project/ProjectList.js
+++ front/src/components/project/ProjectList.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import ProjectElement from './ProjectElement';
+import * as Api from "../../api";
 
-const ProjectList = ({ projectList, setProjectList, isEditable }) => {
+const ProjectList = ({ portfolioOwnerId, projectList, setProjectList, isEditable }) => {
+
+    useEffect(() => {
+        Api.put(`users/${portfolioOwnerId}`, {projectNum: projectList.length});
+    },[portfolioOwnerId, projectList])
+
     return (
         projectList.map((project, index) => (
             <ProjectElement key={index} project={project} isEditable={isEditable} setProjectList={setProjectList}></ProjectElement>
